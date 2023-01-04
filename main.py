ceaa7fd0b009c27a3d9b5e6fa935e4ded81e55e0
Arley Souza <98409215+ArleySouzaDev@users.noreply.github.com>
2023-01-04 17:23:31 +0000

    att exercise

--- main.py
+++ main.py
@@ -3,5 +3,14 @@ height = int(input("What is your height in cm? "))
 
 if height >= 120:
   print(f"okay,your height is {height} you can pass.")
+  
+  age = int(input("How old are you? "))
+  if age < 12:
+    print("please pay $5")
+  elif age <= 18:
+    print("please pay $7")
+  else:
+    print("please pay $12")
+  
 else:
   print("Your height is not enough to go on the toy.")
