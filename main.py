fb7f4d2a9a212460e32a04b807c21ff78ae7574a
Arley Souza <98409215+ArleySouzaDev@users.noreply.github.com>
2023-01-04 14:13:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+  print(f"okay,your height is {height} you can pass.")
+else:
+  print("Your height is not enough to go on the toy.")
